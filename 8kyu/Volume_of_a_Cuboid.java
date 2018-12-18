1b78690cc7e4818ffdb66bba27171d4b0771f98c
Everaldo Rosa de Souza Junior <juniorklawa10@gmail.com>
2018-12-18 09:01:39 -0200

    Create Volume_of_a_Cuboid.java

---
+++ 8kyu/Volume_of_a_Cuboid.java
@@ -0,0 +1,9 @@
+public class Kata {
+
+  public static double getVolumeOfCuboid(final double length, final double width, final double height) {
+    
+    double volume = length * width * height;
+    return volume;
+  }
+  
+}
