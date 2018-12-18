2d0b299a07a5a9bec37e5bd358d9f22d73dbb76b
Everaldo Rosa de Souza Junior <juniorklawa10@gmail.com>
2018-12-18 09:02:03 -0200

    Create Reversed_Strings.java

---
+++ 8kyu/Reversed_Strings.java
@@ -0,0 +1,12 @@
+public class Kata {
+
+  public static String solution(String str) {
+    String reverse = str;
+    
+    StringBuilder sb = new StringBuilder(reverse);
+    
+    
+    return sb.reverse().toString();
+  }
+
+}
