4a8693cb55143eb5719dbe51cefcd4e859208bf6
Everaldo Rosa de Souza Junior <juniorklawa10@gmail.com>
2018-12-18 09:02:47 -0200

    Create Are_you_playing_banjo.java

---
+++ 8kyu/Are_you_playing_banjo.java
@@ -0,0 +1,15 @@
+import java.io.*;
+public class Banjo {
+  public static String areYouPlayingBanjo(String name) {
+    
+
+    
+    if (name.charAt(0) == 'R' || name.charAt(0) == 'r') {
+      return name + " plays banjo";
+    }
+    else {
+      return name + " does not play banjo";
+    }  
+    
+  }
+}
