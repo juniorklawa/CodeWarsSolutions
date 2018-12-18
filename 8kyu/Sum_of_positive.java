aee773f393a12af65f8ce86407926f925f1f3677
Everaldo Rosa de Souza Junior <juniorklawa10@gmail.com>
2018-12-18 09:08:50 -0200

    Create Sum_of_positive.java

---
+++ 8kyu/Sum_of_positive.java
@@ -0,0 +1,16 @@
+public class Positive{
+
+  public static int sum(int[] arr){
+  
+  int total = 0;
+  for(int i = 0 ; i < arr.length; i++){
+  
+    if(arr[i] > 0){
+      total+= arr[i];
+    }
+  
+  }
+    return total;
+  }
+
+}
