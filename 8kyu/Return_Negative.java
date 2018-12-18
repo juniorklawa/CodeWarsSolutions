ee2806068130170825523ef9abacc3c745edd87b
Everaldo Rosa de Souza Junior <juniorklawa10@gmail.com>
2018-12-18 09:00:32 -0200

    Create Return_Negative.java

---
+++ 8kyu/Return_Negative.java
@@ -0,0 +1,14 @@
+public class Kata {
+
+  public static int makeNegative(final int x) {
+  
+      if (x < 0 || x == 0){
+          return x;
+      }
+      else{
+          return x*-1;
+      }
+
+  }
+  
+}
